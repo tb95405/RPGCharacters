6cb81213fcb1b583fa5e47483fe69938ea8954f8
tb95405 <69611134+tb95405@users.noreply.github.com>
2020-09-17 12:21:18 -0700

    Create RPGCharactersRunner.java

---
+++ RPGCharactersRunner.java
@@ -0,0 +1,10 @@
+public class RPGCharactersRunner {
+	
+	public static void main(String[] args) {
+		Warrior c1 = new Warrior("Jim");
+		Warrior c2 = new Warrior("Bob");
+		Warrior c3 = c1;
+		Warrior c4 = new Warrior("Min",5);
+		Warrior c5 = new Warrior("Jeff",7);
+	}
+}
